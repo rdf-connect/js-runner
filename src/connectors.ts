5ed2cd79f7991bfcb3dd32212c0621d13d495381
smessie <smessie@smessie.com>
2024-08-23 14:59:01 +0200

    style: Reformat code with prettier

--- src/connectors.ts
+++ src/connectors.ts
@@ -2,258 +2,275 @@ import { createTermNamespace } from "@treecg/types";
 
 import { NamedNode, Term } from "@rdfjs/types";
 import {
-  FileWriterConfig,
-  startFileStreamReader,
-  startFileStreamWriter,
+    FileWriterConfig,
+    startFileStreamReader,
+    startFileStreamWriter,
 } from "./connectors/file";
 export * from "./connectors/file";
 
 import {
-  startWsStreamReader,
-  startWsStreamWriter,
-  WsWriterConfig,
+    startWsStreamReader,
+    startWsStreamWriter,
+    WsWriterConfig,
 } from "./connectors/ws";
 export * from "./connectors/ws";
 
 import {
-  KafkaReaderConfig,
-  KafkaWriterConfig,
-  startKafkaStreamReader,
-  startKafkaStreamWriter,
+    KafkaReaderConfig,
+    KafkaWriterConfig,
+    startKafkaStreamReader,
+    startKafkaStreamWriter,
 } from "./connectors/kafka";
 export * from "./connectors/kafka";
 
 import {
-  HttpReaderConfig,
-  HttpWriterConfig,
-  startHttpStreamReader,
-  startHttpStreamWriter,
+    HttpReaderConfig,
+    HttpWriterConfig,
+    startHttpStreamReader,
+    startHttpStreamWriter,
 } from "./connectors/http";
 import { LOG } from "./util";
 export * from "./connectors/http";
 
 export const Conn = createTermNamespace(
-  "https://w3id.org/conn#",
-  "FileReaderChannel",
-  "FileWriterChannel",
-  "HttpReaderChannel",
-  "HttpWriterChannel",
-  "KafkaReaderChannel",
-  "KafkaWriterChannel",
-  "WsReaderChannel",
-  "WsWriterChannel",
-  "WriterChannel",
-  "ReaderChannel",
+    "https://w3id.org/conn#",
+    "FileReaderChannel",
+    "FileWriterChannel",
+    "HttpReaderChannel",
+    "HttpWriterChannel",
+    "KafkaReaderChannel",
+    "KafkaWriterChannel",
+    "WsReaderChannel",
+    "WsWriterChannel",
+    "WriterChannel",
+    "ReaderChannel",
 );
 
 export interface Config<T> {
-  id: Term;
-  ty: NamedNode;
-  config: T;
+    id: Term;
+    ty: NamedNode;
+    config: T;
 }
 
 export type ReaderConstructor<C> = (config: C) => {
-  reader: Stream<string | Buffer>;
-  init: () => Promise<void>;
+    reader: Stream<string | Buffer>;
+    init: () => Promise<void>;
 };
 
 export type WriterConstructor<C> = (config: C) => {
-  writer: Writer<string | Buffer>;
-  init: () => Promise<void>;
+    writer: Writer<string | Buffer>;
+    init: () => Promise<void>;
 };
 
 export const JsOntology = createTermNamespace(
-  "https://w3id.org/conn/js#",
-  "JsProcess",
-  "JsChannel",
-  "JsReaderChannel",
-  "JsWriterChannel",
+    "https://w3id.org/conn/js#",
+    "JsProcess",
+    "JsChannel",
+    "JsReaderChannel",
+    "JsWriterChannel",
 );
 type JsChannel = {
-  channel: {
-    id: Term;
-  };
+    channel: {
+        id: Term;
+    };
 };
 
 export class ChannelFactory {
-  private inits: (() => Promise<void>)[] = [];
-  private jsChannelsNamedNodes: { [label: string]: SimpleStream<string> } = {};
-  private jsChannelsBlankNodes: { [label: string]: SimpleStream<string> } = {};
+    private inits: (() => Promise<void>)[] = [];
+    private jsChannelsNamedNodes: { [label: string]: SimpleStream<string> } =
+        {};
+    private jsChannelsBlankNodes: { [label: string]: SimpleStream<string> } =
+        {};
 
-  createReader(config: Config<any>): Stream<string | Buffer> {
-    LOG.channel("Creating reader %s: a %s", config.id.value, config.ty.value);
-    if (config.ty.equals(Conn.FileReaderChannel)) {
-      const { reader, init } = startFileStreamReader(config.config);
-      this.inits.push(init);
+    /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+    createReader(config: Config<any>): Stream<string | Buffer> {
+        LOG.channel(
+            "Creating reader %s: a %s",
+            config.id.value,
+            config.ty.value,
+        );
+        if (config.ty.equals(Conn.FileReaderChannel)) {
+            const { reader, init } = startFileStreamReader(config.config);
+            this.inits.push(init);
 
-      return reader;
-    }
-
-    if (config.ty.equals(Conn.WsReaderChannel)) {
-      const { reader, init } = startWsStreamReader(config.config);
-      this.inits.push(init);
-
-      return reader;
-    }
-
-    if (config.ty.equals(Conn.KafkaReaderChannel)) {
-      const { reader, init } = startKafkaStreamReader(
-        <KafkaReaderConfig>config.config,
-      );
-      this.inits.push(init);
-      return reader;
-    }
-
-    if (config.ty.equals(Conn.HttpReaderChannel)) {
-      const { reader, init } = startHttpStreamReader(
-        <HttpReaderConfig>config.config,
-      );
-      this.inits.push(init);
-      return reader;
-    }
-
-    if (config.ty.equals(JsOntology.JsReaderChannel)) {
-      const c = <JsChannel>config.config;
-      if (c.channel) {
-        const id = c.channel.id.value;
-        if (c.channel.id.termType === "NamedNode") {
-          if (!this.jsChannelsNamedNodes[id]) {
-            this.jsChannelsNamedNodes[id] = new SimpleStream<string>();
-          }
-
-          return this.jsChannelsNamedNodes[id];
+            return reader;
         }
 
-        if (c.channel.id.termType === "BlankNode") {
-          if (!this.jsChannelsBlankNodes[id]) {
-            this.jsChannelsBlankNodes[id] = new SimpleStream<string>();
-          }
+        if (config.ty.equals(Conn.WsReaderChannel)) {
+            const { reader, init } = startWsStreamReader(config.config);
+            this.inits.push(init);
 
-          return this.jsChannelsBlankNodes[id];
-        }
-        throw "Should have found a thing";
-      }
-    }
-    throw "Unknown reader channel " + config.ty.value;
-  }
-
-  createWriter(config: Config<any>): Writer<string | Buffer> {
-    LOG.channel("Creating writer %s: a %s", config.id.value, config.ty.value);
-    if (config.ty.equals(Conn.FileWriterChannel)) {
-      const { writer, init } = startFileStreamWriter(
-        <FileWriterConfig>config.config,
-      );
-      this.inits.push(init);
-
-      return writer;
-    }
-
-    if (config.ty.equals(Conn.WsWriterChannel)) {
-      const { writer, init } = startWsStreamWriter(
-        <WsWriterConfig>config.config,
-      );
-      this.inits.push(init);
-
-      return writer;
-    }
-
-    if (config.ty.equals(Conn.KafkaWriterChannel)) {
-      const { writer, init } = startKafkaStreamWriter(
-        <KafkaWriterConfig>config.config,
-      );
-      this.inits.push(init);
-      return writer;
-    }
-
-    if (config.ty.equals(Conn.HttpWriterChannel)) {
-      const { writer, init } = startHttpStreamWriter(
-        <HttpWriterConfig>config.config,
-      );
-      this.inits.push(init);
-      return writer;
-    }
-
-    if (config.ty.equals(JsOntology.JsWriterChannel)) {
-      const c = <JsChannel>config.config;
-      if (c.channel) {
-        const id = c.channel.id.value;
-        if (c.channel.id.termType === "NamedNode") {
-          if (!this.jsChannelsNamedNodes[id]) {
-            this.jsChannelsNamedNodes[id] = new SimpleStream<string>();
-          }
-
-          return this.jsChannelsNamedNodes[id];
+            return reader;
         }
 
-        if (c.channel.id.termType === "BlankNode") {
-          if (!this.jsChannelsBlankNodes[id]) {
-            this.jsChannelsBlankNodes[id] = new SimpleStream<string>();
-          }
-
-          return this.jsChannelsBlankNodes[id];
+        if (config.ty.equals(Conn.KafkaReaderChannel)) {
+            const { reader, init } = startKafkaStreamReader(
+                <KafkaReaderConfig>config.config,
+            );
+            this.inits.push(init);
+            return reader;
         }
-        throw "Should have found a thing";
-      }
+
+        if (config.ty.equals(Conn.HttpReaderChannel)) {
+            const { reader, init } = startHttpStreamReader(
+                <HttpReaderConfig>config.config,
+            );
+            this.inits.push(init);
+            return reader;
+        }
+
+        if (config.ty.equals(JsOntology.JsReaderChannel)) {
+            const c = <JsChannel>config.config;
+            if (c.channel) {
+                const id = c.channel.id.value;
+                if (c.channel.id.termType === "NamedNode") {
+                    if (!this.jsChannelsNamedNodes[id]) {
+                        this.jsChannelsNamedNodes[id] =
+                            new SimpleStream<string>();
+                    }
+
+                    return this.jsChannelsNamedNodes[id];
+                }
+
+                if (c.channel.id.termType === "BlankNode") {
+                    if (!this.jsChannelsBlankNodes[id]) {
+                        this.jsChannelsBlankNodes[id] =
+                            new SimpleStream<string>();
+                    }
+
+                    return this.jsChannelsBlankNodes[id];
+                }
+                throw "Should have found a thing";
+            }
+        }
+        throw "Unknown reader channel " + config.ty.value;
     }
 
-    throw "Unknown writer channel " + config.ty.value;
-  }
+    /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+    createWriter(config: Config<any>): Writer<string | Buffer> {
+        LOG.channel(
+            "Creating writer %s: a %s",
+            config.id.value,
+            config.ty.value,
+        );
+        if (config.ty.equals(Conn.FileWriterChannel)) {
+            const { writer, init } = startFileStreamWriter(
+                <FileWriterConfig>config.config,
+            );
+            this.inits.push(init);
 
-  async init(): Promise<void> {
-    await Promise.all(this.inits.map((x) => x()));
-  }
+            return writer;
+        }
+
+        if (config.ty.equals(Conn.WsWriterChannel)) {
+            const { writer, init } = startWsStreamWriter(
+                <WsWriterConfig>config.config,
+            );
+            this.inits.push(init);
+
+            return writer;
+        }
+
+        if (config.ty.equals(Conn.KafkaWriterChannel)) {
+            const { writer, init } = startKafkaStreamWriter(
+                <KafkaWriterConfig>config.config,
+            );
+            this.inits.push(init);
+            return writer;
+        }
+
+        if (config.ty.equals(Conn.HttpWriterChannel)) {
+            const { writer, init } = startHttpStreamWriter(
+                <HttpWriterConfig>config.config,
+            );
+            this.inits.push(init);
+            return writer;
+        }
+
+        if (config.ty.equals(JsOntology.JsWriterChannel)) {
+            const c = <JsChannel>config.config;
+            if (c.channel) {
+                const id = c.channel.id.value;
+                if (c.channel.id.termType === "NamedNode") {
+                    if (!this.jsChannelsNamedNodes[id]) {
+                        this.jsChannelsNamedNodes[id] =
+                            new SimpleStream<string>();
+                    }
+
+                    return this.jsChannelsNamedNodes[id];
+                }
+
+                if (c.channel.id.termType === "BlankNode") {
+                    if (!this.jsChannelsBlankNodes[id]) {
+                        this.jsChannelsBlankNodes[id] =
+                            new SimpleStream<string>();
+                    }
+
+                    return this.jsChannelsBlankNodes[id];
+                }
+                throw "Should have found a thing";
+            }
+        }
+
+        throw "Unknown writer channel " + config.ty.value;
+    }
+
+    async init(): Promise<void> {
+        await Promise.all(this.inits.map((x) => x()));
+    }
 }
 
 export interface Writer<T> {
-  push(item: T): Promise<void>;
-  end(): Promise<void>;
+    push(item: T): Promise<void>;
+    end(): Promise<void>;
 }
 
 export interface Stream<T> {
-  lastElement?: T;
-  end(): Promise<void>;
-  data(listener: (t: T) => PromiseLike<void> | void): this;
-  on(event: "data", listener: (t: T) => PromiseLike<void> | void): this;
-  on(event: "end", listener: () => PromiseLike<void> | void): this;
+    lastElement?: T;
+    end(): Promise<void>;
+    data(listener: (t: T) => PromiseLike<void> | void): this;
+    on(event: "data", listener: (t: T) => PromiseLike<void> | void): this;
+    on(event: "end", listener: () => PromiseLike<void> | void): this;
 }
 
 export type Handler<T> = (item: T) => Promise<void> | void;
 
 export class SimpleStream<T> implements Stream<T> {
-  private readonly dataHandlers: Handler<T>[] = [];
-  private readonly endHandlers: Handler<void>[] = [];
+    private readonly dataHandlers: Handler<T>[] = [];
+    private readonly endHandlers: Handler<void>[] = [];
 
-  public readonly disconnect: () => Promise<void>;
-  public lastElement?: T | undefined;
+    public readonly disconnect: () => Promise<void>;
+    public lastElement?: T | undefined;
 
-  public constructor(onDisconnect?: () => Promise<void>) {
-    this.disconnect = onDisconnect || (async () => {});
-  }
-
-  public data(listener: Handler<T>): this {
-    this.dataHandlers.push(listener);
-    return this;
-  }
-
-  public async push(data: T): Promise<void> {
-    this.lastElement = data;
-    await Promise.all(this.dataHandlers.map((handler) => handler(data)));
-  }
-
-  public async end(): Promise<void> {
-    await this.disconnect();
-    await Promise.all(this.endHandlers.map((handler) => handler()));
-  }
-
-  public on(event: "data", listener: Handler<T>): this;
-  public on(event: "end", listener: Handler<void>): this;
-  public on(event: "data" | "end", listener: Handler<any>): this {
-    if (event === "data") {
-      this.dataHandlers.push(listener);
+    public constructor(onDisconnect?: () => Promise<void>) {
+        this.disconnect = onDisconnect || (async () => {});
     }
-    if (event === "end") {
-      this.endHandlers.push(listener);
+
+    public data(listener: Handler<T>): this {
+        this.dataHandlers.push(listener);
+        return this;
     }
-    return this;
-  }
+
+    public async push(data: T): Promise<void> {
+        this.lastElement = data;
+        await Promise.all(this.dataHandlers.map((handler) => handler(data)));
+    }
+
+    public async end(): Promise<void> {
+        await this.disconnect();
+        await Promise.all(this.endHandlers.map((handler) => handler()));
+    }
+
+    public on(event: "data", listener: Handler<T>): this;
+    public on(event: "end", listener: Handler<void>): this;
+    /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+    public on(event: "data" | "end", listener: Handler<any>): this {
+        if (event === "data") {
+            this.dataHandlers.push(listener);
+        }
+        if (event === "end") {
+            this.endHandlers.push(listener);
+        }
+        return this;
+    }
 }
--- src/connectors/http.ts
+++ src/connectors/http.ts
@@ -1,131 +1,131 @@
 import * as http from "http";
 import type * as https from "https";
 import type {
-  IncomingMessage,
-  RequestListener,
-  Server,
-  ServerResponse,
+    IncomingMessage,
+    RequestListener,
+    Server,
+    ServerResponse,
 } from "http";
 import { createServer } from "http";
 import type { Readable } from "stream";
 import {
-  Config,
-  ReaderConstructor,
-  SimpleStream,
-  WriterConstructor,
+    ReaderConstructor,
+    SimpleStream,
+    WriterConstructor,
 } from "../connectors";
 
 function streamToString(
-  stream: Readable,
-  binary: boolean,
+    stream: Readable,
+    binary: boolean,
 ): Promise<string | Buffer> {
-  const datas = <Buffer[]>[];
-  return new Promise((res) => {
-    stream.on("data", (data) => {
-      datas.push(data);
+    const datas = <Buffer[]>[];
+    return new Promise((res) => {
+        stream.on("data", (data) => {
+            datas.push(data);
+        });
+        stream.on("end", () => {
+            const streamData = Buffer.concat(datas);
+            res(binary ? streamData : streamData.toString());
+        });
     });
-    stream.on("end", () => {
-      const streamData = Buffer.concat(datas);
-      res(binary ? streamData : streamData.toString());
-    });
-  });
 }
 
 export interface HttpReaderConfig {
-  endpoint: string;
-  port: number;
-  binary: boolean;
-  waitHandled?: boolean;
-  responseCode?: number;
+    endpoint: string;
+    port: number;
+    binary: boolean;
+    waitHandled?: boolean;
+    responseCode?: number;
 }
 
 export const startHttpStreamReader: ReaderConstructor<HttpReaderConfig> = (
-  config,
+    config,
 ) => {
-  let server: Server;
+    let server: Server | undefined = undefined;
 
-  const stream = new SimpleStream<string | Buffer>(
-    () =>
-      new Promise((res) => {
-        if (server !== undefined) {
-          server.close(() => {
-            res();
-          });
-        } else {
-          res();
+    const stream = new SimpleStream<string | Buffer>(
+        () =>
+            new Promise((res) => {
+                if (server !== undefined) {
+                    server.close(() => {
+                        res();
+                    });
+                } else {
+                    res();
+                }
+            }),
+    );
+
+    const requestListener: RequestListener = async function (
+        req: IncomingMessage,
+        res: ServerResponse,
+    ) {
+        try {
+            const content = await streamToString(req, config.binary);
+
+            const promise = stream.push(content).catch((error) => {
+                throw error;
+            });
+            if (config.waitHandled) {
+                await promise;
+            }
+        } catch (error: unknown) {
+            console.error("Failed", error);
         }
-      }),
-  );
 
-  const requestListener: RequestListener = async function (
-    req: IncomingMessage,
-    res: ServerResponse,
-  ) {
-    try {
-      const content = await streamToString(req, config.binary);
+        res.writeHead(config.responseCode || 200);
+        res.end("OK");
+    };
 
-      const promise = stream.push(content).catch((error) => {
-        throw error;
-      });
-      if (config.waitHandled) {
-        await promise;
-      }
-    } catch (error: unknown) {
-      console.error("Failed", error);
-    }
-
-    res.writeHead(config.responseCode || 200);
-    res.end("OK");
-  };
-
-  server = createServer(requestListener);
-  const init = () => {
-    return new Promise<void>((res) => {
-      const cb = (): void => res(undefined);
-      if (server) {
-        server.listen(config.port, config.endpoint, cb);
-      } else {
-        cb();
-      }
-    });
-  };
-  return { reader: stream, init };
+    server = createServer(requestListener);
+    const init = () => {
+        return new Promise<void>((res) => {
+            const cb = (): void => res(undefined);
+            if (server) {
+                server.listen(config.port, config.endpoint, cb);
+            } else {
+                cb();
+            }
+        });
+    };
+    return { reader: stream, init };
 };
 
 export interface HttpWriterConfig {
-  endpoint: string;
-  method: string;
+    endpoint: string;
+    method: string;
 }
 
 export const startHttpStreamWriter: WriterConstructor<HttpWriterConfig> = (
-  config,
+    config,
 ) => {
-  const requestConfig = <https.RequestOptions>new URL(config.endpoint);
+    const requestConfig = <https.RequestOptions>new URL(config.endpoint);
 
-  const push = async (item: string | Buffer): Promise<void> => {
-    await new Promise(async (resolve) => {
-      const options = {
-        hostname: requestConfig.hostname,
-        path: requestConfig.path,
-        method: config.method,
-        port: requestConfig.port,
-      };
+    const push = async (item: string | Buffer): Promise<void> => {
+        await new Promise((resolve) => {
+            const options = {
+                hostname: requestConfig.hostname,
+                path: requestConfig.path,
+                method: config.method,
+                port: requestConfig.port,
+            };
 
-      const cb = (response: IncomingMessage): void => {
-        response.on("data", () => {});
-        response.on("end", () => {
-          resolve(null);
+            const cb = (response: IncomingMessage): void => {
+                response.on("data", () => {});
+                response.on("end", () => {
+                    resolve(null);
+                });
+            };
+
+            const req = http.request(options, cb);
+            req.write(item, () => {
+                req.end();
+            });
+            // res(null);
         });
-      };
+    };
 
-      const req = http.request(options, cb);
-      await new Promise((res) => req.write(item, res));
-      await new Promise((res) => req.end(res));
-      // res(null);
-    });
-  };
+    const end = async (): Promise<void> => {};
 
-  const end = async (): Promise<void> => {};
-
-  return { writer: { push, end }, init: async () => {} };
+    return { writer: { push, end }, init: async () => {} };
 };
